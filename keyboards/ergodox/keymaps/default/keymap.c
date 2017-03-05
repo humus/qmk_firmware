81ac2d9c357b877158adb32abeb23185192c5668
rbernabe <roberto.bernabe@gmail.com>
2017-03-05 16:04:27 -0600

    add keys, page_down, page up convenient brackets

--- keyboards/ergodox/keymaps/default/keymap.c
+++ keyboards/ergodox/keymaps/default/keymap.c
@@ -32,15 +32,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   Grv  |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |  -/=   |
+ * |   Grv  |   1  |   2  |   3  |   4  |   5  | PGDW |           | PGUP |   6  |   7  |   8  |   9  |   0  |  -/=   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |   Tab  |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   [    |
+ * |   Tab  |   Q  |   W  |   E  |   R  |   T  |ENTER |           |ENTER |   Y  |   U  |   I  |   O  |   P  |   [    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |Ctrl/Esc| A/L1 |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / Cmd |
- * |--------+------+------+------+------+------|ENTER |           |ENTER |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|DELETE|           |BCKSP |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1|  '"  |AltShf| Left | LAlt |                                       | RAlt | RCtrl|   \  |   ]  | ~L1  |
+ *   |LEFT  | DOWN |  UP  | RIGHT| LAlt |                                       | RAlt |      |   [  |   ]  | ENTER |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
@@ -54,20 +54,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_GRV,           KC_1,           KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
-        KC_TAB,           KC_Q,           KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
-  CTL_T(KC_ESC),  LT(SYMB,KC_A),          KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,          CTL_T(KC_Z),    KC_X,   KC_C,   KC_V,   KC_B,   KC_ENT,
-        KC_EQL,KC_QUOT,   LALT(KC_LSFT),  KC_LEFT,KC_LALT,
+        KC_GRV,           KC_1,  KC_2,   KC_3,     KC_4,   KC_5,   KC_PGDN,
+        KC_TAB,           KC_Q,  KC_W,   KC_E,     KC_R,   KC_T,   KC_ENT,
+  CTL_T(KC_ESC),  LT(SYMB,KC_A), KC_S,   KC_D,     KC_F,   KC_G,
+        KC_LSFT,    CTL_T(KC_Z), KC_X,   KC_C,     KC_V,   KC_B,   KC_DELT,
+        KC_LEFT,        KC_DOWN, KC_UP,  KC_RIGHT, KC_LALT,
                                               ALT_T(KC_APP),  KC_LGUI,
                                                               KC_HOME,
                                                KC_SPC,KC_DELT,KC_END,
         // right hand
-             KC_RGHT,     KC_6,   KC_7,     KC_8,    KC_9,   KC_0,             TD(TD_MIN_EQ),
-             TG(SYMB),    KC_Y,   KC_U,     KC_I,    KC_O,   KC_P,             KC_LBRC,
+             KC_PGUP,     KC_6,   KC_7,     KC_8,    KC_9,   KC_0,             TD(TD_MIN_EQ),
+             KC_ENT,      KC_Y,   KC_U,     KC_I,    KC_O,   KC_P,             KC_LBRC,
                           KC_H,   KC_J,     KC_K,    KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_QUOT),
-             KC_ENT,      KC_N,   KC_M,     KC_COMM, KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                                  KC_RALT,  KC_RCTRL,KC_BSLS,KC_RBRC,          KC_FN1,
+             KC_BSPC,      KC_N,   KC_M,     KC_COMM, KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
+                                  KC_RALT,  KC_RCTRL,KC_LBRC,KC_RBRC,          KC_ENT,
              KC_LALT,        CTL_T(KC_ESC),
              KC_PGUP,
              KC_PGDN,KC_ENT, KC_SPC
